1feed5f9c6c9241b2dbf37ae5c26cd56a55b05eb
Marek Urban <marek.urban.enterprise@gmail.com>
2022-07-12 00:03:29 +0200

    fix: missing bootstrap package

--- newtify-backend/src/main/java/sk/best/newtify/backend/bootstrap/NewtifyBackendApplication.java
+++ newtify-backend/src/main/java/sk/best/newtify/backend/bootstrap/NewtifyBackendApplication.java
@@ -1,4 +1,4 @@
-package sk.best.newtify.backend;
+package sk.best.newtify.backend.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
